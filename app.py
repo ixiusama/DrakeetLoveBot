4526e25b300a2fcfa8218d8beac7252fcb639b35
ixiusama <ixiusama@hotmail.com>
2016-07-06 11:43:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,11 @@ bot_name = '@XiaoaiBot'
 global bot
 # 由于 美国节点，只能 git 部署，我不得不开源 token，请不要随便拿去用，如需生成你自己的 token
 # 请联系 http://telegram.me/BotFather 谢谢！
-bot = telegram.Bot(token='194363679:AAEUbDAhPiq-Y_6dmwhkHmWvaBj1pTfRDKc')
-songci_api = 'http://api.jisuapi.com/songci/search?appkey=7528478e273bd00b&keyword='
+bot = telegram.Bot(token='193304113:AAHKVL_0Z7dLQ4vDj5PleqDZ5Z7KKjP1uJA')
 
 @app.route('/')
 def index():
-    return r'{"drakeet":"hehe"}'
+    return r'{"xiaoyue_bot":"hehe"}'
 
 
 @app.route('/<token>', methods=['POST'])
@@ -77,11 +76,10 @@ def handle_message(message):
 
 def help(message):
     text = ('/echo - Repeat the same message back\n'
-            '/milestone - Get drakeet\'s milestone\n'
+            '/milestone - Get xiaoyue_bot\'s milestone\n'
             '/getmylastat - Get my last @ message\n'
             '/pic - Curiosity killed the cat\n'
-            '/delpic - Delete pic by its num\n'
-            '/songci - TEXT')
+            '/delpic - Delete pic by its num')
     bot.sendMessage(chat_id=message.chat.id, text=text)
 
 
@@ -137,7 +135,7 @@ def echo(message):
 def milestone(message):
     from_day = datetime(2013, 7, 16)
     now = datetime.now()
-    text = 'drakeet 和他家老婆大人已经认识并相爱 %d 天啦（此处应该有恭喜' % (now - from_day).days
+    text = 'xiaoyue_bot 和他家老婆大人已经认识并相爱 %d 天啦（此处应该有恭喜' % (now - from_day).days
     chat_id = message.chat.id
     bot.sendMessage(chat_id=chat_id, text=text)
 
@@ -243,40 +241,6 @@ def delpic(message):
         pic.set('pid', text)
         pic.save()
     send_successful(message)
-
-
-def songci(message):
-    cmd, text = parse_cmd_text(message.text)
-    if text == None or len(text) == 0:
-        bot.sendMessage(chat_id=message.chat.id,
-                        reply_to_message_id=message.message_id,
-                        text='请使用 /songci <词名>')
-        return
-    bot.sendChatAction(chat_id=message.chat.id, action=telegram.ChatAction.TYPING)
-    text = text.replace(' ', '·')
-    keyword = urllib2.quote(text)
-    response = urllib2.urlopen(songci_api + keyword)
-    data = json.loads(response.read())
-    Songci = Object.extend('Songci')
-    __songci = Songci()
-    __songci.set('keyword', keyword)
-    __songci.set('data', response.read())
-    __songci.save()
-    try:
-        a_songci =  data['result']['list'][0]
-    except TypeError as e:
-        bot.sendMessage(chat_id=message.chat.id,
-                        reply_to_message_id=message.message_id,
-                        text='找不到对应的宋词')
-        return
-    __text = a_songci['title'] + '\n' + a_songci['author'] + '\n' + a_songci['content']
-    block_chars = '⓪①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳❶❷❸❹❺❻❼❽❾❿⑴⑵⑶⑷⑸⑹⑺⑻⑼⑽⑾⑿⒀⒁⒂⒃⒄⒅⒆⒇'
-    temp = ''
-    for c in __text:
-        if not c in block_chars:
-            temp += c
-    __text = temp.replace('&nbsp;', ' ').replace('<br />', '\n')
-    bot.sendMessage(chat_id=message.chat.id, text=__text)
 
 
 Haha = Object.extend('Haha')
